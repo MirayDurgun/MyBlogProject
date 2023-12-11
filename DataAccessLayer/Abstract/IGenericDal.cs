a0d059eac8be6f9756a14a3968dbbca655238be8
Miray Durgun <miraydurgun67@gmail.com>
2023-12-11 17:22:41 +0300

    Dal katmanı Abstract oluşturuldu generic yapıda

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    { //CRUD
+        void Insert(T t);
+        void Update(T t);
+        void Delete(T t);
+        List<T> GetListAll(); //void yok
+                              //GetAll da yazılabilir
+        T GetById(int id);  //dışarıdan id parametresi alır.
+        List<T> GetListAll(Expression<Func<T, bool>> filter); //// Filtreleme kriterine uyan öğeleri döndürür.
+    }
+}
