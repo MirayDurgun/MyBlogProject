4c44fc01927df86d321efc6a0a9f5b8dfbc53486
Miray Durgun <miraydurgun67@gmail.com>
2023-12-12 19:24:18 +0300

    portfolio için gerekli düzenlemeler yapıldı

--- DataAccessLayer/EntityFramework/EfProjectRepository.cs
+++ DataAccessLayer/EntityFramework/EfProjectRepository.cs
@@ -1,6 +1,8 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.Repositories;
 using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +11,14 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.EntityFramework
 {
-    public class EfProjectRepository:GenericRepository<Project>, IProjectDal
+    public class EfProjectRepository : GenericRepository<Project>, IProjectDal
     {
-
+        public List<Project> GetListWithPortfolio()
+        {
+           using (var c = new Context())
+            {
+                return c.Projects.Include(c => c.Portfolio).ToList();
+            }
+        }
     }
 }
