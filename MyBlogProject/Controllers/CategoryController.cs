5d2268cf31654dbf3131f2014ce943b87938a9d0
Miray Durgun <miraydurgun67@gmail.com>
2023-12-12 19:37:40 +0300

    category index oluşturuldu

---
+++ MyBlogProject/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index()
+        {
+            var values = cm.GetList();
+            //cm. içindeki bütün metotlara erişim sağlansın
+        
+
+            return View(values);
+            //geriye valuesi döndür
+        }
+    }
+}
