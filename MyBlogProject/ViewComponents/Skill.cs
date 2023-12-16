3e23c396f57ab20d3e71ace4c44b37d3fc83b524
Miray Durgun <miraydurgun67@gmail.com>
2023-12-16 23:21:33 +0300

    skill components eklendi

---
+++ MyBlogProject/ViewComponents/Skill.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.ViewComponents.Skill
+{
+	public class Skill : ViewComponent
+	{
+        SkillManager sm = new SkillManager(new EfSkillRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = sm.GetList();
+            return View(values);
+        }
+    }
+}
