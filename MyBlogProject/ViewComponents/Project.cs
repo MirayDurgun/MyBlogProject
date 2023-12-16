c63e8c6e4e4e50def17f312415a76f170b4d2699
Miray Durgun <miraydurgun67@gmail.com>
2023-12-16 23:33:11 +0300

    project component yapıldı

---
+++ MyBlogProject/ViewComponents/Project.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.ViewComponents.Project
+{
+	public class Project : ViewComponent
+	{
+        ProjectManager pm = new ProjectManager(new EfProjectRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = pm.GetList();
+            return View(values);
+        }
+    }
+}
