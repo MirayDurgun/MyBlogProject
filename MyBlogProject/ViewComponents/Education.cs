a1c44a9b99e5bd51348ca2b98730b67a36915e26
Miray Durgun <miraydurgun67@gmail.com>
2023-12-16 22:43:29 +0300

    education components yapıldı

---
+++ MyBlogProject/ViewComponents/Education.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBlog.ViewComponents.Education
+{
+	public class Education : ViewComponent
+	{
+        EducationManager em = new EducationManager(new EfEducationRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = em.GetList();
+            return View(values);
+        }
+    }
+}
