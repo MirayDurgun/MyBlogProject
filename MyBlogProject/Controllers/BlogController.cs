42e1bef48716cb81fa6c1b5deef09c046a1fdb72
Miray Durgun <miraydurgun67@gmail.com>
2023-12-20 23:26:56 +0300

    controllerdaki hata düzeltildi. single sayfası için random yazılımcı sözleri eklendi yan panele

--- MyBlogProject/Controllers/BlogController.cs
+++ MyBlogProject/Controllers/BlogController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyBlogProject.Controllers
@@ -12,13 +13,18 @@ namespace MyBlogProject.Controllers
         {
             var values = bm.GetBlogListWithCategory();
             return View(values);
+
+
         }
         public IActionResult BlogSingle(int id)
         {
             //idye göre detayları getirecek
             ViewBag.i = id; //yorum için
-            var values = bm.GetById(id);
+            var values = bm.GetBlogById(id);
             return View(values);
+
         }
+
+
     }
 }
