1a8ea4fa585861c4be7bea3da9d1bdcb1e660a3c
Miray Durgun <miraydurgun67@gmail.com>
2024-02-06 02:26:54 +0300

    Admin Education sayfası yapıldı ve listelendi

---
+++ MyBlogProject/Areas/Admin/Controllers/AdminEducationController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminEducationController : Controller
+    {
+        EducationManager em = new EducationManager(new EfEducationRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyBlogProject/ViewComponents/AdminEducation.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.ViewComponents
+{
+    public class AdminEducation : ViewComponent
+    {
+        EducationManager em = new EducationManager(new EfEducationRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = em.GetList();
+            return View(values);
+        }
+    }
+}
