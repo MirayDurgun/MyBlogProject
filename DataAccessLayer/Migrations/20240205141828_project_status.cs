6a840dab02fad26668f918b284e257fd47224bb7
Miray Durgun <miraydurgun67@gmail.com>
2024-02-05 17:51:13 +0300

    projeye status eklendi. admin proje index sayfası yapıldı

---
+++ DataAccessLayer/Migrations/20240205141828_project_status.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class project_status : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ProjectStatus",
+                table: "Projects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryID",
+                table: "Blogs",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectStatus",
+                table: "Projects");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryID",
+                table: "Blogs",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Blogs_Categories_CategoryID",
+                table: "Blogs",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryID");
+        }
+    }
+}
--- MyBlogProject/Areas/Admin/Controllers/AdminProjectController.cs
+++ MyBlogProject/Areas/Admin/Controllers/AdminProjectController.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MyBlogProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AdminProjectController : Controller
     {
+        ProjectManager pm = new ProjectManager(new EfProjectRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = pm.GetList();
+            return View(values);
         }
     }
 }
