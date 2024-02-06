8638ca8548d61a9a3c3454b0159fd7755f96f3fa
Miray Durgun <miraydurgun67@gmail.com>
2024-02-06 15:19:26 +0300

    portfolio için sertifika listeleme yapıldı

---
+++ MyBlogProject/ViewComponents/AdminCertificate.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace MyBlogProject.ViewComponents
+{
+    public class AdminCertificate : ViewComponent
+    {
+        CertificateManager cm = new CertificateManager(new EfCertificateRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = cm.GetList();
+            return View(values);
+        }
+    }
+}
