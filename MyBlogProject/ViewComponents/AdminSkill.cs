34d4d3bc9897b403c0ef160a210a00ad2a43ff91
Miray Durgun <miraydurgun67@gmail.com>
2024-02-08 21:55:23 +0300

    admin skill sayfası yapıldı, listelendi

---
+++ MyBlogProject/ViewComponents/AdminSkill.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.ViewComponents
+{
+    public class AdminSkill : ViewComponent
+    {
+        SkillManager sm = new SkillManager(new EfSkillRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = sm.GetList();
+            return View(values);
+        }
+    }
+}
