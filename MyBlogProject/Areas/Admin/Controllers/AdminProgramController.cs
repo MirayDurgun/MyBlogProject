b8ae62c7b8f9d1e992586190ccd4f36b684b4758
Miray Durgun <miraydurgun67@gmail.com>
2024-02-09 18:15:51 +0300

    Ajax ile program ekleme yapıldı

---
+++ MyBlogProject/Areas/Admin/Controllers/AdminProgramController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminProgramController : Controller
+    {
+        ProgramManager pm = new ProgramManager(new EfProgramRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ProgramAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ProgramAdd(EntityLayer.Concrete.Program p)
+        {
+
+            //if (results.IsValid)
+            //{
+            pm.TAdd(p);
+            return RedirectToAction("Index", "AdminPortfolio");
+            //}
+            //else
+            //{
+            //    foreach (var itpm in results.Errors)
+            //    {
+            //        ModelState.AddModelError(itpm.PropertyName, itpm.ErrorMessage);
+            //    }
+            //    return View();
+            //}
+        }
+
+    }
+}
