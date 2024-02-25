6c5a23d2f0adc9be776b3417053dddd79b8c1b33
Miray Durgun <miraydurgun67@gmail.com>
2024-02-26 01:05:22 +0300

    admin testimonial index listeleme yapıldı

---
+++ MyBlogProject/ViewComponents/AdminTestimonial.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.ViewComponents
+{
+    public class AdminTestimonial : ViewComponent
+    {
+        TestimonialManager tm = new TestimonialManager(new EfTestimonialRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+            var values = tm.GetList();
+            return View(values);
+        }
+    }
+}
