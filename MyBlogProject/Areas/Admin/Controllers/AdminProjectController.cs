7aa281b697418009c23b03d18f5b1214d2b08ab2
Miray Durgun <miraydurgun67@gmail.com>
2024-03-01 21:21:01 +0300

    Admin Project için aktif pasif işlemi yapıldı. Project index bu status'e göre listelenecek

--- MyBlogProject/Areas/Admin/Controllers/AdminProjectController.cs
+++ MyBlogProject/Areas/Admin/Controllers/AdminProjectController.cs
@@ -60,6 +60,19 @@ namespace MyBlogProject.Areas.Admin.Controllers
             return RedirectToAction("Index", "AdminProject");
         }
 
+        public IActionResult AdminProjectActive(int id, bool status)
+        // İlgili blogun kimliği (id) ve yeni durumu (status) parametre olarak alındı.
+        {
+            var adminblogvalue = pm.GetById(id);
+            // Blogun etkinlik durumunu, gelen 'status' parametresiyle günceller.
+            adminblogvalue.ProjectStatus = status;
+            //status değişkeni, bu durumun yeni değerini taşır
+            pm.TUpdate(adminblogvalue);
+
+            return RedirectToAction("Index", "AdminProject");
+
+        }
+
     }
 }
 
