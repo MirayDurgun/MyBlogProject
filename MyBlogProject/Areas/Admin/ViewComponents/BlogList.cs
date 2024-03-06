d857b7031cf97d41309c24bac77be543e091c1d1
Miray Durgun <miraydurgun67@gmail.com>
2024-03-06 23:12:04 +0300

    admin index için son bloglar alanı yapıldı

---
+++ MyBlogProject/Areas/Admin/ViewComponents/BlogList.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.Areas.Admin.ViewComponents
+{
+    public class BlogList : ViewComponent
+    {
+        BlogManager bm=new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+           var values= bm.GetLast5Blog();
+            return View(values);
+        }
+    }
+}
+
