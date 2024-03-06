73eaf41eb30b638033136017441e4eb4a0b99356
Miray Durgun <miraydurgun67@gmail.com>
2024-03-06 23:37:00 +0300

    Admin index için proje listeleme alanı yapıldı

---
+++ MyBlogProject/Areas/Admin/ViewComponents/ProjectList.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyProjectProject.Areas.Admin.ViewComponents
+{
+    public class ProjectList : ViewComponent
+    {
+        ProjectManager pm=new ProjectManager(new EfProjectRepository());
+        public IViewComponentResult Invoke(int id)
+        {
+           var values= pm.GetLast8Project();
+            return View(values);
+        }
+    }
+}
+
