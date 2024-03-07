d7c843f5441b829de5ff6448af5f9a7d46f4828d
Miray Durgun <miraydurgun67@gmail.com>
2024-03-07 23:28:31 +0300

    Footer alanı için proje listeleme eklendi

---
+++ MyBlogProject/ViewComponents/FooterProject.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlogProject.ViewComponents.Project
+{
+	public class FooterProject : ViewComponent
+	{
+		
+		ProjectManager cm = new ProjectManager(new EfProjectRepository());
+		public IViewComponentResult Invoke(int id)
+        {
+            var values = cm.GetLast8Project();
+			return View(values);
+		}
+	}
+}
