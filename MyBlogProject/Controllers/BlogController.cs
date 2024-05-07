02284e6fddb47892984312e6d127fc7a32fda82a
Miray Durgun <miraydurgun67@gmail.com>
2024-05-07 21:11:58 +0300

    düzenleme

--- MyBlogProject/Controllers/BlogController.cs
+++ MyBlogProject/Controllers/BlogController.cs
@@ -3,6 +3,7 @@ using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace MyBlogProject.Controllers
 {
@@ -10,24 +11,25 @@ namespace MyBlogProject.Controllers
     public class BlogController : Controller
     {
         BlogManager bm = new BlogManager(new EfBlogRepository());
-
-
+        CommentManager cm = new CommentManager(new EfCommentRepository());
 
         public IActionResult Index()
         {
             var values = bm.GetBlogListWithCategory();
             return View(values);
-
-
         }
+
         public IActionResult BlogSingle(int id)
         {
+            var values = bm.GetBlogById(id);
             //idye göre detayları getirecek
             ViewBag.BlogID = id; //yorum için
-            var values = bm.GetBlogById(id);
+
+            var comments = cm.GetList().Where(c => c.BlogID == id).ToList();
+            // Yorum sayısını alıyoruz
+            ViewBag.CommentCount = comments.Count;
             return View(values);
+
         }
-
-
     }
 }
